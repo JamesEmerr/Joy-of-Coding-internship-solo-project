1bc6f58379274f3d98e1d447c0022c6feb1cb131
JamesEMerr <jamesEmerr@gmail.com>
2024-02-05 21:39:46 -0600

    Layout update

--- Solo-Project/src/App.tsx
+++ Solo-Project/src/App.tsx
@@ -1,11 +1,20 @@
 import { Grid, GridItem } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return<Grid templateAreas={'"nav nav" "aside main"'}>
-    <GridItem area='nav' bg='coral'>Navigation Bar</GridItem>
-    <GridItem area='aside' bg='grey'>Side Panel</GridItem>
-    <GridItem area='main' bg='blue'>Main</GridItem>
-  </Grid>
+  return (
+    <Grid templateAreas={'"nav nav" "aside main"'}>
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>    
+      <GridItem area="aside" bg="grey">
+        Side Panel
+      </GridItem>
+      <GridItem area="main" bg="blue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
---
+++ Solo-Project/src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import { HStack, Text } from "@chakra-ui/react";
+import { px } from "framer-motion";
+
+const NavBar = () => {
+  return (
+    <HStack>
+      <Text boxSize="105px" fontSize="5xl" >MCX</Text>
+    </HStack>
+  );
+};
+
+export default NavBar;
